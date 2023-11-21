04ee093b37c1b3a56ad423fdf80c2ca6bef6e6a3
ashiqsultan <ashiqdrive@gmail.com>
2023-11-22 00:40:24 +0530

    Added general vector search

--- src/services/product/searchProducts.ts
+++ src/services/product/searchProducts.ts
@@ -1,14 +1,45 @@
-import { IProductDocument } from '../../models/products';
+import ProductCollection, { IProductDocument } from '../../models/products';
 import generateEmbedding from '../../utils/openai/generateEmbedding';
 
 const searchProducts = async (
   searchText: string
 ): Promise<IProductDocument[]> => {
-  // Generate Embedding
-  const embedding = await generateEmbedding(searchText);
-  // Query DB
-  // Return Result
-  return [];
+  try {
+    // Generate Embedding
+    const embedding = await generateEmbedding(searchText);
+    const collection = await ProductCollection();
+    // Query DB
+    const aggCursor = collection.aggregate<IProductDocument>([
+      {
+        $vectorSearch: {
+          index: 'mynewvectorindex',
+          path: 'embedding',
+          queryVector: embedding,
+          numCandidates: 150,
+          limit: 2,
+        },
+      },
+      // { $match: { category: 'Electronics', price: { $lte: 200 } } },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          category: 1,
+          description: 1,
+          price: 1,
+          score: { $meta: 'vectorSearchScore' },
+        },
+      },
+    ]);
+    const products: IProductDocument[] = [];
+    // Print the aggregated results
+    for await (const doc of aggCursor) {
+      products.push(doc);
+    }
+    return products;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default searchProducts;
